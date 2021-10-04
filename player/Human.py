2cdff2e4854b77b23e65d2e3378ee0a1004b080a
Michael Couch <michael@michaelcouch.io>
2021-10-04 22:59:32 +1100

    Added a human player (types moves)

---
+++ player/Human.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from Player import Player
+
+
+class Human(Player):
+    """A class implementing a human player manually taking turns
+    """
+
+    def __init__(self, id):
+        Player.__init__(self, id)
+
+    def select_move(self, observation):
+        """Allow the human player to select a move for the game
+
+        :observation: Observation of the game
+        :returns: move listed in observations
+
+        """
+        print(f"It's player {self.id}'s turn.")
+        print("Scores are:")
+        for player_id, score in observation['scores']:
+            print(f"{player_id}: {score}")
+
+        print("The game looks like")
+        print(observation['observation'])
+
+        return self.read_move(observation['moves'])
+
+    def get_move(self, moves):
+        """Get the next player move, with checking
+
+        :moves: list of strings
+        :returns: element in moves
+
+        """
+        valid_move = False
+        while not valid_move:
+            move = input(f"Please enter a valid move ({moves}): ")
+            if move in moves:
+                return move
