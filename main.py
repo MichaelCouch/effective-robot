cf6287fe1430d67db7cd0d9e4414011767035545
Michael Couch <michael@michaelcouch.io>
2021-10-04 23:29:41 +1100

    Added working game with 2 human players to main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
-from game.Game import Game
+from games import CoinFlip
+from players import Human
 
+players = [Human('Lily'), Human('Michael')]
+
+game = CoinFlip(players)
+
+end_scores = game.run_game()
+print(end_scores)
