54ce0a5c9331a477100e736aeb2560bf181ba058
Michael Couch <michael@michaelcouch.io>
2021-10-24 15:09:03 +1100

    Move the next move selector and neural network updater to helper functions

--- players/SimpleNeuralNet.py
+++ players/SimpleNeuralNet.py
@@ -39,61 +39,48 @@ class SimpleNeuralNet(Player):
         self._last_score = None
         self._last_action = None
 
-    def select_move(self, observation):
-        # Update known state data
-        for var in observation['observations']:
-            if var['name'] in self._variables:
-                self._input[self._variables.index(var['name']),1] = var['value']
+    def _update_network(self, observation):
+        """ Update the neural network based on the new score we see
+        """
+        ## Update the neural network based on our new score
+        previous_state_action_value = self._output['activation'][self._actions.index(self._last_action)]
+        new_score = observation['scores'][self._id]
+        points_awarded = self._last_score = new_score
 
-        if self._last_score and self._last_action:
-            ## Update the neural network based on our new score
-            previous_state_action_value = self._output['activation'][self._actions.index(self._last_action)]
-            new_score = observation['scores'][self._id]
-            points_awarded = self._last_score = new_score
-
-            # Update the state
-            self._update_activations()
-            current_state_value = np.max(self._output['activation'])
-
-            error = points_awarded - (
-                previous_state_action_value - self._discount * current_state_value
-            )
-            update_vector = np.zeros(self._output['activation'].shape)
-            update_vector[self._actions.index(self._last_action),1] = error
-
-            # Update network weights and biases by gradient descent
-
-
-            dp2 = self._dp2(self._Wo.dot(self._hidden['activation']) - self._output['bias'])
-            dp1 = self._dp1(self._Wi.dot(self._input) - self._hidden['bias'])
-
-            dWo = np.tensordot(dp2, self._hidden['activation'],axes=(1,1))
-            dWi = np.tensordot(np.transpose(dp2 * self._Wo) * dp1, self._input, axes=0)
-            dob = - dp2
-            dhb = - np.transpose(dp2 * self._Wo) * dp1
-
-            self._Wi += self._learning_rate * np.tensordot(dWi, update_vector,axes=([1,3], [0,1]))
-            self._hidden['bias'] += self._learning_rate * np.tensordot(dhb, update_vector, axes=(1,0))
-            self._Wo += self._learning_rate * dWo * update_vector
-            self._output['bias'] += self._learning_rate * dob * update_vector
-
-
-        ## Add data of any never-before-seen moves and state data
-        actions = observation['moves']
-        for action in actions:
-            if action not in self._actions:
-                self._add_action(action)
-        variables = observation['observation']
-        for var in variables:
-            if var['name'] not in self._variables:
-                assert isinstance(var['value'], float) or isinstance(var['value'],int), 'Only accepting atomic numbers for SimpleNeuralNet'
-                self._add_variable(var)
-
-        ## Select the next move to make
+        # Update the state
         self._update_activations()
+        current_state_value = np.max(self._output['activation'])
+
+        error = points_awarded - (
+            previous_state_action_value - self._discount * current_state_value
+        )
+        update_vector = np.zeros(self._output['activation'].shape)
+        update_vector[self._actions.index(self._last_action),1] = error
+
+        # Update network weights and biases by gradient descent
+        dp2 = self._dp2(self._Wo.dot(self._hidden['activation']) - self._output['bias'])
+        dp1 = self._dp1(self._Wi.dot(self._input) - self._hidden['bias'])
+
+        dWo = np.tensordot(dp2, self._hidden['activation'],axes=(1,1))
+        dWi = np.tensordot(np.transpose(dp2 * self._Wo) * dp1, self._input, axes=0)
+        dob = - dp2
+        dhb = - np.transpose(dp2 * self._Wo) * dp1
+
+        self._Wi += self._learning_rate * np.tensordot(dWi, update_vector,axes=([1,3], [0,1]))
+        self._hidden['bias'] += self._learning_rate * np.tensordot(dhb, update_vector, axes=(1,0))
+        self._Wo += self._learning_rate * dWo * update_vector
+        self._output['bias'] += self._learning_rate * dob * update_vector
+
+    def _select_valid_move(self, obsercation):
+        """Use activation levels to randomly select a valid move to make.
+
+        :obsercation: TODO
+        :returns: a valid moce
+
+        """
         # Not all actions we know about are valid so restrict
-        valid = [action in actions for action in self._actions]
-        moves = [action for action in self._actions if action in actions]
+        valid = [action in observation['moves'] for action in self._actions]
+        moves = [action for action in self._actions if action in observation['moves']]
 
         # Use activation levels to pick a move randomly
         cdf = np.cumsum(softmax(self._output['activation'][valid]))
@@ -102,8 +89,33 @@ class SimpleNeuralNet(Player):
         while r > cdf[move]:
             move += 1
 
+        return self._actions[move]
+
+    def select_move(self, observation):
+        # Update known state data
+        for var in observation['observations']:
+            if var['name'] in self._variables:
+                self._input[self._variables.index(var['name']),1] = var['value']
+
+        ## Update the neural network based on our new score
+        if self._last_score and self._last_action:
+            self._update_network(observation)
+
+        ## Add data of any never-before-seen moves and state data
+        for action in observation['moves']:
+            if action not in self._actions:
+                self._add_action(action)
+        for var in observation['observation']:
+            if var['name'] not in self._variables:
+                assert isinstance(var['value'], float) or isinstance(var['value'],int), 'Only accepting atomic numbers for SimpleNeuralNet'
+                self._add_variable(var)
+
+        ## Select the next move to make
+        self._update_activations()
+        next_action = self._select_valid_move(observation)
+
         self._last_score = observation['scores'][self._id]
-        self._last_action = self._actions[move]
+        self._last_action = next_action
         return self._last_action
 
 
