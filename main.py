4bdb66cf0eba07b544f5b10889a62bddfafda65b
Michael Couch <michael@michaelcouch.io>
2021-10-25 23:18:24 +1100

    Add Neural net as player, loop gameplay

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
+from time import sleep
 from games import CoinFlip
 from games import CartPole
 from players import Human
 from players import RandomPlayer
+from players import SimpleNeuralNet
 
-players = [Human("Lily"), Human("Michael"), RandomPlayer("Lilith")]
+players = [
+    SimpleNeuralNet('C3PO', n=2),
+    # Human("Lily"),
+    # Human("Michael"),
+    RandomPlayer("Lilith")
+]
 
-game = CartPole(players)
 
-end_scores = game.run_game()
+playing = True
+n = 0
+while playing:
+    game = CartPole(players)
+    game.run_game()
+    #if input('Play again?') != 'y':
+    #    playing=False
+    sleep(1000/1000)
+    n+=1
 print(end_scores)
